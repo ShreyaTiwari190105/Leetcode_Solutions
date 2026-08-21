b019144ef05fe02a9a5ed83b1bf47773a1762452
Shreya Tiwari <shreyat190105@gmail.com>
2026-08-21 11:29:08 +0530

    Time: 7 ms (40.75%) | Memory: 19.3 MB (58.07%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {'I' : 1 ,'V' : 5 ,'X' : 10 ,'L' : 50 ,'C': 100 ,'D' :500
+        ,'M' : 1000}
+
+        total = 0
+        
+        for i in range(len(s) -1):
+            current_value = d[s[i]]
+            next_value = d[s[i+1]]
+
+            if current_value < next_value :
+                total -= current_value
+            else:
+                total += current_value
+
+        total += d[s[-1]]
+
+        return total
+        
